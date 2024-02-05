408befb55a6995bb8206378e29f3deb92425b8f3
thekamleshyadav <thekamleshyadav26@gmail.com>
2024-02-05 18:04:54 +0530

    add github-actions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,13 @@
 terraform {
-  required_version = ">= 1.6.6"
-
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 5.13.0"
+      version = ">= 4.75, < 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.75, < 5.13.0"
     }
   }
 }
